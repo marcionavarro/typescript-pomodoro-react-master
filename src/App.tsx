8b43cf08b60372a05a4f3d6dce4aa3505f6ffe9f
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-06-01 19:03:26 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PomodoroTimer } from './components/pomodoro-timer';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      Olá Mundo
+    </div>
+  );
+}
+
+export default App;
